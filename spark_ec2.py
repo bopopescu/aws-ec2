4297fd94f1b50867eea15029bd3ddf73e142ae18
murphy.wang <wyz_sun@126.com>
2017-07-21 15:56:00 +0800

    ec2.py

--- spark_ec2.py
+++ spark_ec2.py
@@ -105,8 +105,8 @@ DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
 DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
-DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/amplab/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-1.6"
+DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/murphy-wang/aws-ec2"
+DEFAULT_SPARK_EC2_BRANCH = "mk_version"
 
 
 def setup_external_libs(libs):
